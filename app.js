a1c07ca36f1c0e27d82713ee4b42f2ed0dc87caf
FrontEnd-develop-it <kutsenko.valeriia.work@gmail.com>
2024-12-01 18:19:18 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('open-menu').addEventListener('click', () => {
+	document.querySelector('.menu').classList.toggle('open');
+})
+
+document.getElementById('open-search-input').addEventListener('click', () => {
+	document.getElementById('search').classList.toggle('open');
+})
